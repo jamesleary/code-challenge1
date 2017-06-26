a97a60966dfbec2984d946f2120773458a457455
jamesleary96@yahoo.com <jamesleary96@yahoo.com>
2017-06-26 13:42:46 -0500

    Added Swap and Delete buttons within div elements appending to the DOM

--- client.js
+++ client.js
@@ -3,11 +3,18 @@ numClicks = 0;
 
 $(document).ready(function(){
   console.log('jquery sourced');
+generateClick();
 
+
+}); //end of document.ready
+
+function generateClick (){
   $('#generate').on('click', function(){
     console.log('generate click');
     numClicks += 1;
     $('.container').empty();
-    $('.container').append('<div><p>'+ numClicks+ '</p></div>');
+    $('.container').append('<div id ="moreButtons"><p>' + numClicks +
+        '<button>Swap</button>' + '<button>Delete</button>'+ '</p></div>');
+
   });
-}); //end of document.ready
+}
